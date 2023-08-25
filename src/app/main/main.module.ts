cafdc59db4c0a8c1d2283f6d32b7b670aceec9f3
Susom <susomshrestha@gmail.com>
2023-08-25 11:21:31 -0400

    add fontawesome icons add navbar for mobile and large screen

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { MainRoutingModule } from './main-routing.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, MainRoutingModule],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, MainRoutingModule, FontAwesomeModule],
+  exports: [HeaderComponent]
 })
 export class MainModule {}
---
+++ src/app/main/shared/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  isScrolled = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+  }
+
+}
