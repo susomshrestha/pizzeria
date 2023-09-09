0ba7219efb23bb28d1a9bbf535320a5509dc7c20
Susom <susomshrestha@gmail.com>
2023-09-09 09:14:50 -0400

    add selected class in side menu on route change

--- src/app/admin/shared/components/admin-header/admin-header.component.ts
+++ src/app/admin/shared/components/admin-header/admin-header.component.ts
@@ -1,22 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
   styleUrls: ['./admin-header.component.scss'],
 })
-export class AdminHeaderComponent {
+export class AdminHeaderComponent implements OnInit, OnDestroy {
   @Output() collapseEvent = new EventEmitter<boolean>();
 
   collapse: boolean = false;
 
+  routerSubscription: Subscription;
+  currentRoute: String = '';
+
   constructor(private router: Router) {
-    console.log(router.url)
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
   }
+
+  ngOnInit(): void {}
 
   toggleCollapse() {
     this.collapse = !this.collapse;
     this.collapseEvent.emit(this.collapse);
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
   }
 }
